2c07326baaabbcd4e3a57300014011417dbb4e42
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-11-21 22:54:11 +0100

    ass Inconsistent dataset in the mock file

--- src/__mock__/data.mock.js
+++ src/__mock__/data.mock.js
@@ -33,6 +33,43 @@ export const releasenotes = {
     ]
   }
 };
+// Inconsistent dataset
+export const apm = {
+  filename: 'apm.json',
+  content: {
+    categories: ['Use Cases'],
+    pages: [
+      {
+        d: '/apm/use-cases/',
+        i: 'index',
+        t: 'Use Cases',
+        u: '/apm/use-cases/'
+      },
+      {
+        d: '/apm/use-cases/uc-1/',
+        i: 'generic',
+        p: 'use-cases-1',
+        t: 'Generic',
+        u: '/apm/use-cases/uc-1/generic'
+      },
+      {
+        c: 'Use Cases',
+        d: '/apm/use-cases/uc-1/',
+        i: 'use-cases-1',
+        mo: 20,
+        t: 'APM 1 Use Cases',
+        u: '/apm/use-cases/uc-1/use-cases-1'
+      },
+      {
+        d: '/apm/use-cases/uc-1/',
+        i: 'in-development',
+        p: 'use-cases-1',
+        t: 'In Development',
+        u: '/apm/use-cases/uc-1/in-development'
+      }
+    ]
+  }
+};
 
 const data = [releasenotes];
 
