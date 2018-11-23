b42851227ffa7aac27a482018df9608ed6a607fe
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-11-23 22:01:05 +0100

    Improved naming in walk.js

--- src/components/TreeViewMenu/walk.js
+++ src/components/TreeViewMenu/walk.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 import ListGroupItem from './ListGroupItem';
 
-const getItem = ({
+const walk = (data, props) =>
+  Object.entries(data)
+    .sort((a, b) => a[1].index - b[1].index)
+    .reduce(
+      (all, [nodeName, node]) => [
+        ...all,
+        ...(node.key ? generateBranch(node, nodeName, props) : [])
+      ],
+      []
+    );
+
+const generateBranch = (node, nodeName, props) => {
+  const { parent = '', level = 0, openNodes, searchTerm } = props;
+  const { nodes } = node;
+  const currentNode = [parent, nodeName].filter(x => x).join('/');
+  const showChildren =
+    !!nodes && (openNodes.includes(currentNode) || searchTerm);
+
+  const currentItem = getComponent({
+    showChildren,
+    currentNode,
+    ...props,
+    ...node
+  });
+  const nextLevelItems = showChildren
+    ? walk(nodes, { ...props, parent: currentNode, level: level + 1 })
+    : [];
+
+  return [currentItem, ...nextLevelItems].filter(x => x);
+};
+
+const getComponent = ({
   activeKey,
   key,
   showChildren,
@@ -35,31 +66,5 @@ const getItem = ({
     )
   );
 };
-
-const getNodeComponent = (node, nodeName, props) => {
-  const { parent = '', level = 0, openNodes, searchTerm } = props;
-  const { nodes } = node;
-  const currentNode = [parent, nodeName].filter(x => x).join('/');
-  const showChildren =
-    !!nodes && (openNodes.includes(currentNode) || searchTerm);
-
-  const currentItem = getItem({ showChildren, currentNode, ...props, ...node });
-  const nextLevelItems = showChildren
-    ? walk(nodes, { ...props, parent: currentNode, level: level + 1 })
-    : [];
-
-  return [currentItem, ...nextLevelItems].filter(x => x);
-};
-
-const walk = (data, props) =>
-  Object.entries(data)
-    .sort((a, b) => a[1].index - b[1].index)
-    .reduce(
-      (all, [nodeName, node]) => [
-        ...all,
-        ...(node.key ? getNodeComponent(node, nodeName, props) : [])
-      ],
-      []
-    );
 
 export default walk;
