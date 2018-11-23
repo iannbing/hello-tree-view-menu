ac02bc15c47ee398f1d868ed2889a5cd20a8edfa
Huang-Ming Chang <huang.m.chang@gmail.com>
2018-11-23 15:18:06 +0100

    Fixed the routing bug

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ const AppRoutes = () => (
   <Router history={history}>
     <Suspense fallback={<div />}>
       <Switch>
-        <Route exact path="/" component={() => <DemoPage />} />
-        <Route path="/:path" component={() => <DemoPage />} />
+        <Route path="/:path?" component={() => <DemoPage />} />
       </Switch>
     </Suspense>
   </Router>
